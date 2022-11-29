3f3df303fe6280638057ba70207582f0a2dd4f94
Stephen Brown <stephenrbrownnb@gmail.com>
2022-11-29 18:29:43 -0400

    hardcoded cities and countries

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,5 +26,24 @@ $( function() {
 
   $(".searchBtn").click(function(){
     city = $("#city").val();
-    console.log(city);
+   
+    if (city=="Miami") {
+   console.log("Miami") ;
+   visitCountry = "United States";
+    } else if (city=="Cancun"){
+      console.log("Cancun");
+      visitCountry = "Mexico";
+    }
+    else if (city=="Paris"){
+      console.log("Paris ");
+      visitCountry="France";
+    }
+    else if (city=="London"){
+      console.log("london");
+      visitCountry="England";
+    }
+    else if (city=="Los Angelas"){
+      console.log("Los Angelas");
+      viitCountry="United States";
+    }
   });
