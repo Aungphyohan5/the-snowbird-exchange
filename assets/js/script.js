6564ccff6cd195d1354b758e5e0932eae36ab195
Stephen Brown <stephenrbrownnb@gmail.com>
2022-11-29 18:17:33 -0400

    Added search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 
+let locations = [
+  {city:"Miami", country:"United States"},
+  {city:"Cancun", country:"Mexico"},
+  {city:"Paris", country:"France"},
+  {city:"London", country:"England"},
+  {city:"Los Angelas", country:"United States"}
+];
+
 //This is the autocomplete for the search bar. 
 $( function() {
     var availableCities = [
+      
       "Miami",
       "Cancun",
       "Paris",
@@ -13,3 +22,9 @@ $( function() {
       source: availableCities
     });
   } );
+
+
+  $(".searchBtn").click(function(){
+    city = $("#city").val();
+    console.log(city);
+  });
